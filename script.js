bc7348dd8aecb5c2126670bf40a96720b3a1777c
Johirul Islam Himel <himel1199@gmail.com>
2026-03-07 02:42:01 +0600

    complete dynamic issue cards with status images and priority colors

---
+++ script.js
@@ -0,0 +1,99 @@
+const allIssues = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+
+const issuesContainer = document.getElementById("issues-container");
+const totalCount = document.getElementById("total-count");
+const loadingSpinner = document.getElementById("loading-spinner");
+
+// spinner manage function
+function showLoading() {
+  loadingSpinner.classList.remove("hidden");
+  issuesContainer.innerHTML = "";
+}
+function hideLoading() {
+  loadingSpinner.classList.add("hidden");
+}
+
+// load data
+async function loadIssues() {
+  showLoading();
+  const res = await fetch(allIssues);
+  const data = await res.json();
+  console.log(data.data);
+
+  hideLoading();
+  displayIssues(data.data);
+}
+
+// showing data in UI:
+function displayIssues(issues) {
+  totalCount.innerText = issues.length;
+  issuesContainer.innerHTML = "";
+
+  issues.forEach((issue) => {
+    const card = document.createElement("div");
+
+    // image logic:
+    let statusImage = "";
+    if (issue.status === "open") {
+      statusImage = "./assets/Open-Status.png";
+    } else {
+      statusImage = "./assets/Closed- Status .png";
+    }
+
+    // border color logic:
+    let topBorderColor = "border-t-purple-400"; //
+    if (issue.priority === "high") {
+      topBorderColor = "border-t-emerald-400";
+    } else if (issue.priority === "medium") {
+      topBorderColor = "border-t-yellow-400";
+    }
+
+    // badge color:
+    let badgeTextColor = "text-gray-500";
+    if (issue.priority === "high") {
+      badgeTextColor = "text-red-400";
+    } else if (issue.priority === "medium") {
+      badgeTextColor = "text-yellow-400";
+    }
+
+    // Assign logic:
+    let issueAssignee = "";
+    if (issue.assignee) {
+      issueAssignee = issue.assignee;
+    } else {
+      issueAssignee = "Unassigned";
+    }
+    card.className = `card bg-white shadow-sm border border-gray-200 border-t-4 ${topBorderColor} p-5 flex flex-col gap-3 cursor-pointer hover:shadow-md transition-all`;
+
+    card.innerHTML = `
+      <div class="flex justify-between items-center">
+        <div>
+          <img src="${statusImage}" alt="${issue.status}" class="w-5 h-5">
+        </div>
+        <div class="badge badge-sm bg-white border-gray-200 text-[10px] px-2 py-2 font-bold lowercase italic ${badgeTextColor}">
+           ${issue.priority}
+        </div>
+      </div>
+      
+      <div>
+        <h2 class="text-slate-900 font-bold text-base mb-2 hover:text-blue-600 cursor-pointer transition-colors">${issue.title}</h2>
+        <p class="text-gray-500 text-xs line-clamp-2">${issue.description}</p>
+      </div>
+      <div class="flex flex-wrap gap-2 mb-2">
+        ${issue.labels.map((label) => `<span class="px-2 py-0.5 bg-yellow-400 text-[9px] font-black rounded uppercase">${label}</span>`).join("")}
+      </div>
+      <div class="border-t border-gray-100 pt-3 mt-auto text-[11px] text-gray-400">
+          <div class="flex justify-between mb-1">
+            <p>#${issue.id} by <span class="text-gray-600 font-medium">${issue.author}</span></p>
+            <p>${issue.createdAt.split("T")[0]}</p>
+          </div>
+          <div class="flex justify-between">
+            <p>Assignee: <span class="text-gray-600">${issueAssignee}</span></p>
+            <p>Updated: ${issue.updatedAt.split("T")[0]}</p>
+          </div>
+      </div>
+    `;
+    issuesContainer.appendChild(card);
+  });
+}
+loadIssues();
